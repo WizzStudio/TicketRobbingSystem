78d46eb9b72e5ec6d7682de2f8165144feb711e7
ACERY1 <y841034081@gmail.com>
2018-05-08 18:00:28 +0800

    config sass & npm scripts

--- js/app/main.js
+++ js/app/main.js
@@ -1,6 +1,6 @@
 define((require) => {
 	let request = require('./request')
 	let $ = require('jquery')
+
 	request.sayHello()
-	console.log($('.test'))
 });
--- js/app/request.js
+++ js/app/request.js
@@ -1,9 +1,24 @@
 /* API Layer */
+define((require) => {
+	const ajax = require('jquery').ajax
+	const baseUrl = ''
+	const request = (url = '/', method = 'get') => {
+		return new Promise((resolve, reject) => {
+			ajax({
+				url: baseUrl + url,
+				method: method,
+				success: (data) => {
+					resolve(data)
+				},
+				error: err => (reject(err))
+			})
+		})
+	}
 
-define(() => {
+
 	return {
 		sayHello: () => {
-			console.log('this is the module')
+			return request('/test', 'get')
 		}
 	}
 })
