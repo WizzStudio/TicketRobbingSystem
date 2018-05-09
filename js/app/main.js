540a9e68a6caf6c5a1b69fdc94cd465e4a1e51e3
ACERY1 <y841034081@gmail.com>
2018-05-09 10:43:22 +0800

    add theme & congfig some dialog

--- js/app/main.js
+++ js/app/main.js
@@ -1,11 +1,26 @@
 define((require) => {
-	const request = require('./request')
 	const $ = require('jquery')
+	const validator = require('validator') // 输入校验库
+	const request = require('./request')
 	const al = require('./dialog')
 	const util = require('./utils')
+
+	// 改变主题
+	const changeTheme = () => {
+		$('.js-theme-config').addClass('theme-dark')
+	}
+
+
+	const _init = () => {
+
+	}
+
 	// request.sayHello()
 	// al.showHello()
 	util.autoCalcHeight($('#js-main'))
 	// util.changeBg($('#js-main'),'https://avatars2.githubusercontent.com/u/768052?v=4')
 	// $('#js-main').css('height', '10rem')
+	util.changeText($('#js-act-title'), 'hello world')
+	$('#js-loader').hide()
+	changeTheme()
 });
--- js/app/utils.js
+++ js/app/utils.js
@@ -1,4 +1,46 @@
 define((require) => {
+	const vali = require('validator')
+	const al = require('./dialog')
+	/**
+	 *  存储本地STORE
+	 * @param name
+	 * @param content
+	 */
+	const setStore = (name, content) => {
+		if (!name) return;
+		if (typeof content !== 'string') {
+			content = JSON.stringify(content);
+		}
+		window.localStorage.setItem(name, content);
+	}
+
+	/**
+	 * 获取本地STORE
+	 * @param name 存贮的名字
+	 */
+	const getStore = name => {
+		if (!name) return;
+		return window.localStorage.getItem(name);
+	}
+
+	/**
+	 * 删除本地STORE
+	 * @param name
+	 */
+	const removeStore = name => {
+		if (!name) return;
+		window.localStorage.removeItem(name);
+	}
+
+	/**
+	 * 清除所有的localStorage
+	 */
+	const removeAllStore = () => {
+		for (let i of Object.keys(window.sessionStorage)) {
+			window.localStorage.removeItem(i)
+		}
+	}
+
 	// 动态计算高度
 	const autoCalcHeight = ($dom) => {
 		$dom.css('height', window.innerHeight)
@@ -10,8 +52,30 @@ define((require) => {
 		$dom.css('background-size', 'cover')
 	}
 
+	// 动态改变文本
+	const changeText = ($dom, text) => {
+		$dom.text(text)
+	}
+
+	// 存储个人信息到localStorage
+	const setInfo = (name, tel, stuNum) => {
+		if (!vali.isMobilePhone(tel, 'zh-CN')) al.showError('存储失败！', '非电话号码格式')
+		if (!vali.isNumeric(stuNum)) al.showError('存储失败！', '学号出现非数字')
+		setStore('name', name)
+		setStore('tel', tel)
+		setStore('stuNum', stuNum)
+	}
+
+	// 清除信息
+	const clearInfo = () => {
+		removeAllStore()
+	}
+
 	return {
 		autoCalcHeight,
-		changeBg
+		changeBg,
+		changeText,
+		clearInfo,
+		setInfo
 	}
 });
